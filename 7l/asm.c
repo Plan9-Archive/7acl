9883ce913fd08d503ef5e46b3525b09ceb0ce120
Charles Forsyth <charles.forsyth@gmail.com>
2014-04-18 17:22:07 +0100

    elf64, not elf32

--- 7l/asm.c
+++ 7l/asm.c
@@ -160,7 +160,7 @@ asmb(void)
 		break;
 	case 7:	/* elf */
 		debug['S'] = 1;			/* symbol table */
-		elf32(ARM64, ELFDATA2LSB, 0, nil);
+		elf64(ARM64, ELFDATA2LSB, 0, nil);
 		break;
 	}
 	cflush();
